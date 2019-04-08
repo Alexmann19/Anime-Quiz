7a589dd75d066905a684bda3de5dcfbba34a82cb
Alexander Lehr <Alexlehr@hotmail.de>
2019-04-08 06:50:18 +0200

    add Programm

---
+++ Anime Quiz/Anime Quiz/Anime Quiz/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Anime_Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rtbfrage = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbnummer = new System.Windows.Forms.TextBox();
+            this.tba = new System.Windows.Forms.TextBox();
+            this.tbb = new System.Windows.Forms.TextBox();
+            this.tbc = new System.Windows.Forms.TextBox();
+            this.tbd = new System.Windows.Forms.TextBox();
+            this.rba = new System.Windows.Forms.RadioButton();
+            this.rbb = new System.Windows.Forms.RadioButton();
+            this.rbc = new System.Windows.Forms.RadioButton();
+            this.rbd = new System.Windows.Forms.RadioButton();
+            this.tbpunkte = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbanime = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbfrage
+            // 
+            this.rtbfrage.Location = new System.Drawing.Point(12, 67);
+            this.rtbfrage.Name = "rtbfrage";
+            this.rtbfrage.ReadOnly = true;
+            this.rtbfrage.ShortcutsEnabled = false;
+            this.rtbfrage.Size = new System.Drawing.Size(405, 144);
+            this.rtbfrage.TabIndex = 0;
+            this.rtbfrage.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Frage:";
+            // 
+            // tbnummer
+            // 
+            this.tbnummer.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbnummer.Location = new System.Drawing.Point(55, 13);
+            this.tbnummer.Name = "tbnummer";
+            this.tbnummer.ReadOnly = true;
+            this.tbnummer.ShortcutsEnabled = false;
+            this.tbnummer.Size = new System.Drawing.Size(55, 48);
+            this.tbnummer.TabIndex = 2;
+            // 
+            // tba
+            // 
+            this.tba.Location = new System.Drawing.Point(92, 247);
+            this.tba.Name = "tba";
+            this.tba.ReadOnly = true;
+            this.tba.ShortcutsEnabled = false;
+            this.tba.Size = new System.Drawing.Size(325, 20);
+            this.tba.TabIndex = 3;
+            // 
+            // tbb
+            // 
+            this.tbb.Location = new System.Drawing.Point(92, 273);
+            this.tbb.Name = "tbb";
+            this.tbb.ReadOnly = true;
+            this.tbb.ShortcutsEnabled = false;
+            this.tbb.Size = new System.Drawing.Size(325, 20);
+            this.tbb.TabIndex = 5;
+            // 
+            // tbc
+            // 
+            this.tbc.Location = new System.Drawing.Point(92, 299);
+            this.tbc.Name = "tbc";
+            this.tbc.ReadOnly = true;
+            this.tbc.ShortcutsEnabled = false;
+            this.tbc.Size = new System.Drawing.Size(325, 20);
+            this.tbc.TabIndex = 6;
+            // 
+            // tbd
+            // 
+            this.tbd.Location = new System.Drawing.Point(92, 325);
+            this.tbd.Name = "tbd";
+            this.tbd.ReadOnly = true;
+            this.tbd.ShortcutsEnabled = false;
+            this.tbd.Size = new System.Drawing.Size(325, 20);
+            this.tbd.TabIndex = 7;
+            // 
+            // rba
+            // 
+            this.rba.AutoSize = true;
+            this.rba.Location = new System.Drawing.Point(12, 248);
+            this.rba.Name = "rba";
+            this.rba.Size = new System.Drawing.Size(74, 17);
+            this.rba.TabIndex = 8;
+            this.rba.TabStop = true;
+            this.rba.Text = "Antwort A:";
+            this.rba.UseVisualStyleBackColor = true;
+            // 
+            // rbb
+            // 
+            this.rbb.AutoSize = true;
+            this.rbb.Location = new System.Drawing.Point(12, 274);
+            this.rbb.Name = "rbb";
+            this.rbb.Size = new System.Drawing.Size(74, 17);
+            this.rbb.TabIndex = 9;
+            this.rbb.TabStop = true;
+            this.rbb.Text = "Antwort B:";
+            this.rbb.UseVisualStyleBackColor = true;
+            // 
+            // rbc
+            // 
+            this.rbc.AutoSize = true;
+            this.rbc.Location = new System.Drawing.Point(12, 300);
+            this.rbc.Name = "rbc";
+            this.rbc.Size = new System.Drawing.Size(74, 17);
+            this.rbc.TabIndex = 10;
+            this.rbc.TabStop = true;
+            this.rbc.Text = "Antwort C:";
+            this.rbc.UseVisualStyleBackColor = true;
+            // 
+            // rbd
+            // 
+            this.rbd.AutoSize = true;
+            this.rbd.Location = new System.Drawing.Point(12, 326);
+            this.rbd.Name = "rbd";
+            this.rbd.Size = new System.Drawing.Size(75, 17);
+            this.rbd.TabIndex = 11;
+            this.rbd.TabStop = true;
+            this.rbd.Text = "Antwort D:";
+            this.rbd.UseVisualStyleBackColor = true;
+            // 
+            // tbpunkte
+            // 
+            this.tbpunkte.Location = new System.Drawing.Point(317, 412);
+            this.tbpunkte.Name = "tbpunkte";
+            this.tbpunkte.ReadOnly = true;
+            this.tbpunkte.ShortcutsEnabled = false;
+            this.tbpunkte.Size = new System.Drawing.Size(100, 20);
+            this.tbpunkte.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 415);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Punkte:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 81);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Antwort wählen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbanime
+            // 
+            this.tbanime.Location = new System.Drawing.Point(200, 41);
+            this.tbanime.Name = "tbanime";
+            this.tbanime.ReadOnly = true;
+            this.tbanime.ShortcutsEnabled = false;
+            this.tbanime.Size = new System.Drawing.Size(217, 20);
+            this.tbanime.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(155, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Anime:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 444);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbanime);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbpunkte);
+            this.Controls.Add(this.rbd);
+            this.Controls.Add(this.rbc);
+            this.Controls.Add(this.rbb);
+            this.Controls.Add(this.rba);
+            this.Controls.Add(this.tbd);
+            this.Controls.Add(this.tbc);
+            this.Controls.Add(this.tbb);
+            this.Controls.Add(this.tba);
+            this.Controls.Add(this.tbnummer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbfrage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbfrage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbnummer;
+        private System.Windows.Forms.TextBox tba;
+        private System.Windows.Forms.TextBox tbb;
+        private System.Windows.Forms.TextBox tbc;
+        private System.Windows.Forms.TextBox tbd;
+        private System.Windows.Forms.RadioButton rba;
+        private System.Windows.Forms.RadioButton rbb;
+        private System.Windows.Forms.RadioButton rbc;
+        private System.Windows.Forms.RadioButton rbd;
+        private System.Windows.Forms.TextBox tbpunkte;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbanime;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Anime Quiz/Anime Quiz/Anime Quiz/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anime_Quiz
+{
+    public partial class Form1 : Form
+    {
+        public static int game = 1;
+        public static int answer = 0;
+        public static int youranswer = -1;
+        public static int punkte = 0;
+        public static int weiter = 0;
+        public static string richtig = "";
+        public static int punktea = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tbnummer.Text = "1";
+            tbanime.Text = "Pretty Cure";
+            rtbfrage.Text = "Welche Pretty Cure ist die erste die in Heartcatch Precure erwacht?";
+            tba.Text = "Cure Black";
+            tbb.Text = "Cure Sunshine";
+            tbc.Text = "Cure Blossom";
+            tbd.Text = "Cure Whip";
+            answer = 3;
+            punkte = 5;
+            weiter = 2;
+            richtig = tbc.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rba.Checked == true)
+            {
+                youranswer = 1;
+            }
+            else
+            if (rbb.Checked == true)
+            {
+                youranswer = 2;
+            }
+            else
+            if (rbc.Checked == true)
+            {
+                youranswer = 3;
+            }
+            else
+            if (rbd.Checked == true)
+            {
+                youranswer = 4;
+            }
+            else
+            {
+                MessageBox.Show("Keine Antwort gewählt!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            if (answer == youranswer)
+            {
+                punktea = punktea + punkte;
+                tbpunkte.Text = Convert.ToString(punktea);
+                MessageBox.Show("Richtige Antwort", "Richtig", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                game = weiter;
+            }
+            else
+            {
+                MessageBox.Show("Falsche Antwort", "Falsch", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Richtig ist: " + richtig, "Antwort", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                game = weiter;
+            }
+
+            
+
+                switch (game)
+            {
+                case 1:
+                    tbnummer.Text = "1";
+                    tbanime.Text = "Pretty Cure";
+                    rtbfrage.Text = "Welche Pretty Cure ist die erste die in Heartcatch Precure erwacht?";
+                    tba.Text = "Cure Black";
+                    tbb.Text = "Cure Sunshine";
+                    tbc.Text = "Cure Blossom";
+                    tbd.Text = "Cure Whip";
+                    answer = 3;
+                    punkte = 5;
+                    weiter = 2;
+                    richtig = tbc.Text;
+                    break;
+
+                case 2:
+                    tbnummer.Text = "2";
+                    tbanime.Text = "Pretty Cure";
+                    rtbfrage.Text = "Welche Precure hat Baron Salamander im Heartchatch Precure Film versiegelt?";
+                    tba.Text = "Cure White";
+                    tbb.Text = "Cure Ange";
+                    tbc.Text = "Cure Egret";
+                    tbd.Text = "Cure Moonlight";
+                    answer = 2;
+                    punkte = 20;
+                    weiter = 3;
+                    richtig = tbb.Text;
+                    break;
+
+                case 3:
+                    tbnummer.Text = "3";
+                    tbanime.Text = "Pretty Cure";
+                    rtbfrage.Text = "Wie heist die erste Attake aus der ersten Pretty Cure Staffel (Deutsch)?";
+                    tba.Text = "Marmorstrahl";
+                    tbb.Text = "Regenbogenorkan";
+                    tbc.Text = "Hyperstahl";
+                    tbd.Text = "Macht des Lichts";
+                    answer = 1;
+                    punkte = 10;
+                    weiter = 4;
+                    richtig = tba.Text;
+                    break;
+
+                case 4:
+                    tbnummer.Text = "4";
+                    tbanime.Text = "Pretty Cure";
+                    rtbfrage.Text = "Welche von diesen Pretty Cure ist nach der Verwandlung erwachsen?";
+                    tba.Text = "Cure Heart";
+                    tbb.Text = "Cure Mint";
+                    tbc.Text = "Cure Star";
+                    tbd.Text = "Cure Ace";
+                    answer = 4;
+                    punkte = 10;
+                    weiter = 5;
+                    richtig = tbd.Text;
+                    break;
+
+                case 5:
+                    tbnummer.Text = "5";
+                    tbanime.Text = "Pretty Cure";
+                    rtbfrage.Text = "Wie heist die Pretty Cure Staffel die anfang 2019 rauskam?";
+                    tba.Text = "Precure Max Heart";
+                    tbb.Text = "Star☆Twinkle Pretty Cure";
+                    tbc.Text = "Heartcatch Precure";
+                    tbd.Text = "Yes Precure 5 Go Go";
+                    answer = 2;
+                    punkte = 15;
+                    weiter = 6;
+                    richtig = tbb.Text;
+                    break;
+
+                case 6:
+                    tbnummer.Text = "6";
+                    tbanime.Text = "Pokemon";
+                    rtbfrage.Text = "Wie heist der \"Gott\" der Pokemon (Stand 2019)?";
+                    tba.Text = "Arceus";
+                    tbb.Text = "Pikachu";
+                    tbc.Text = "Dialga";
+                    tbd.Text = "Mew Two";
+                    answer = 1;
+                    punkte = 5;
+                    weiter = 7;
+                    richtig = tba.Text;
+                    break;
+
+                case 7:
+                    tbnummer.Text = "7";
+                    tbanime.Text = "Pokemon";
+                    rtbfrage.Text = "Welches Pokemon hat 8 verschiedene Entwicklungen?";
+                    tba.Text = "Mauzi";
+                    tbb.Text = "Evoli";
+                    tbc.Text = "Bisasam";
+                    tbd.Text = "Gelatini";
+                    answer = 2;
+                    punkte = 5;
+                    weiter = 8;
+                    richtig = tbb.Text;
+                    break;
+
+                case 8:
+                    tbnummer.Text = "8";
+                    tbanime.Text = "Pokemon";
+                    rtbfrage.Text = "Welches Pokemon ist sogar im Uber Tier gebannt?";
+                    tba.Text = "Dragoran";
+                    tbb.Text = "Arceus";
+                    tbc.Text = "Mew Two";
+                    tbd.Text = "Mega-Rayquaza";
+                    answer = 4;
+                    punkte = 15;
+                    weiter = 9;
+                    richtig = tbd.Text;
+                    break;
+
+                case 9:
+                    tbnummer.Text = "9";
+                    tbanime.Text = "Pokemon";
+                    rtbfrage.Text = "Wie heist der Wasserstarter aus Sinnoh?";
+                    tba.Text = "Plinfa";
+                    tbb.Text = "Karnimarni";
+                    tbc.Text = "Schiggi";
+                    tbd.Text = "Froxy";
+                    answer = 1;
+                    punkte = 15;
+                    weiter = 10;
+                    richtig = tba.Text;
+                    break;
+
+                case 10:
+                    tbnummer.Text = "10";
+                    tbanime.Text = "Pokemon";
+                    rtbfrage.Text = "In welcher Region spielt die 7. Generation?";
+                    tba.Text = "Sinnoh";
+                    tbb.Text = "Johto";
+                    tbc.Text = "Hoenn";
+                    tbd.Text = "Alola";
+                    answer = 4;
+                    punkte = 15;
+                    weiter = 11;
+                    richtig = tbd.Text;
+                    break;
+
+                case 11:
+                    tbnummer.Text = "11";
+                    tbanime.Text = "Sailor Moon";
+                    rtbfrage.Text = "Wie heist der letzte Gegner der Serie?";
+                    tba.Text = "Metalia";
+                    tbb.Text = "Köigen Beryl";
+                    tbc.Text = "Sailor Galaxia";
+                    tbd.Text = "Black Lady";
+                    answer = 3;
+                    punkte = 10;
+                    weiter = 12;
+                    richtig = tbc.Text;
+                    break;
+
+                case 12:
+                    tbnummer.Text = "12";
+                    tbanime.Text = "Sailor Moon";
+                    rtbfrage.Text = "Wie heist die Sailor Kriegerin der Stille bzw. des Lebens?";
+                    tba.Text = "Sailor Uranus";
+                    tbb.Text = "Sailor Saturn";
+                    tbc.Text = "Sailor Neptun";
+                    tbd.Text = "Sailor Pluto";
+                    answer = 2;
+                    punkte = 20;
+                    weiter = 13;
+                    richtig = tbb.Text;
+                    break;
+
+                case 13:
+                    tbnummer.Text = "13";
+                    tbanime.Text = "Sailor Moon";
+                    rtbfrage.Text = "Wie heist der Pegasus in der Dead Moon Staffel?";
+                    tba.Text = "Helios";
+                    tbb.Text = "Jedite";
+                    tbc.Text = "Prince Diamond";
+                    tbd.Text = "Pegasus";
+                    answer = 1;
+                    punkte = 15;
+                    weiter = 14;
+                    richtig = tba.Text;
+                    break;
+
+                case 14:
+                    tbnummer.Text = "14";
+                    tbanime.Text = "Sailor Moon";
+                    rtbfrage.Text = "Wie heist die mächtigste Waffe der ersten Staffel?";
+                    tba.Text = "Mondstein";
+                    tbb.Text = "Mondstab";
+                    tbc.Text = "Schwert des Vollmonds";
+                    tbd.Text = "Magischer Silberkristall";
+                    answer = 4;
+                    punkte = 10;
+                    weiter = 15;
+                    richtig = tbd.Text;
+                    break;
+
+                case 15:
+                    tbnummer.Text = "15";
+                    tbanime.Text = "Sailor Moon";
+                    rtbfrage.Text = "Wie heisen die 3 Verbündeten die in der letzten Staffel hinzu kommen?";
+                    tba.Text = "Die dreisten Drei";
+                    tbb.Text = "Das Trio des Lichts";
+                    tbc.Text = "Starlights";
+                    tbd.Text = "Der Bund der Mächtigen";
+                    answer = 3;
+                    punkte = 15;
+                    weiter = 16;
+                    richtig = tbc.Text;
+                    break;
+
+                case 16:
+                    tbnummer.Text = "-";
+                    tbanime.Text = "-";
+                    rtbfrage.Text = "-";
+                    tba.Text = "-";
+                    tbb.Text = "-";
+                    tbc.Text = "-";
+                    tbd.Text = "-";
+
+                    string punktes = Convert.ToString(punktea);
+                    MessageBox.Show("Du hast " + punktes + " Punkte", "Ende", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (punktea <= 30)
+                    {
+                        MessageBox.Show("Du hast ja gar keine Ahnung, Schande über dich, du kannst ja nichtmal Boku no Piko von Dragonball unterscheiden!", "Bewertung", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    if (punktea <= 70 & punktea > 30)
+                    {
+                        MessageBox.Show("Naja du bist ein Anime Neuling darauf kann man aufbauen!", "Bewertung", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    if (punktea <= 130 & punktea > 70)
+                    {
+                        MessageBox.Show("Du kennst dich ja schon sehr gut mit Animes aus, spitze!", "Bewertung", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    if (punktea < 185 & punktea > 130)
+                    {
+                        MessageBox.Show("Bei den sieben Dragonballs, du bist ja ein richtiger Anime Experte - es ist aber noch Luft nach oben!", "Bewertung", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    if (punktea == 185)
+                    {
+                        MessageBox.Show("D..D..Du bist einfach perfekt, du bist der größte Anime Meister der ganzen Welt!", "Bewertung", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    this.Close();
+                    break;
+
+
+                default:
+                    tbnummer.Text = "N/A";
+                    tbanime.Text = "N/A";
+                    rtbfrage.Text = "Error";
+                    tba.Text = "Error";
+                    tbb.Text = "Error";
+                    tbc.Text = "Error";
+                    tbd.Text = "Error";
+                    answer = 0;
+                    punkte = 0;
+                    weiter = 1;
+                    richtig = "N/A";
+                    break;
+
+
+            }
+        }
+
+    }
+}
